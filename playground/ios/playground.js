c8922eb38d3fce880b5e533a22201e7e04c05b05
Darío Javier Cravero <dario@uxtemple.com>
2015-05-05 10:14:01 +0100

    Added js for playground in ios

---
+++ playground/ios/playground.js
@@ -0,0 +1,1 @@
+console.error("\n/Users/craverod/opensource/panels/panels-feed/playground/ios/playground.es6:1\nimport DUMMY_DATA from '../../dummy-data';\n^\nParseError: 'import' and 'export' may appear only with 'sourceType: module'");
